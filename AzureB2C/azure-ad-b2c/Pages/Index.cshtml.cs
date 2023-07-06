ae1aa8e6cd21f60a25d960d372720acda3408c84
Angelo Dejaeghere <angelo@g3l.consulting>
2023-07-06 11:09:44 +0200

    Initial commit

---
+++ AzureB2C/azure-ad-b2c/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_ad_b2c.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
