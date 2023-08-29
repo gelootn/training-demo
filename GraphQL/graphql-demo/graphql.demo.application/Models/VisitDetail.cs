81f51b41c94a4927a579df6fb0ea0e24fd14383d
Angelo Dejaeghere <angelo@g3l.consulting>
2023-08-29 10:10:16 +0200

    uyguyg

--- GraphQL/graphql-demo/graphql.demo.application/Models/VisitDetail.cs
+++ GraphQL/graphql-demo/graphql.demo.application/Models/VisitDetail.cs
@@ -4,7 +4,7 @@ public class VisitDetail
 {
     public string VisitorName { get; set; }
     public string VisitorEmail { get; set; }
-    public string VisitorCompany { get; set; }
+    public string? VisitorCompany { get; set; }
     public string VisitingCompany { get; set; }
     public string VisitingEmployee { get; set; }
     public DateTime StartTime { get; set; }
--- GraphQL/graphql-demo/graphql.demo.application/Queries/VisitQueryType.cs
+++ GraphQL/graphql-demo/graphql.demo.application/Queries/VisitQueryType.cs
@@ -8,25 +8,12 @@ namespace graphql.demo.application.Queries;
 
 public class VisitQueryType
 {
-    [UsePaging(DefaultPageSize = 20, IncludeTotalCount = true)]
-    [UseFiltering]
+    [UsePaging(DefaultPageSize = 20, IncludeTotalCount = true, Order = 1)]
+    [UseFiltering()]
     public IQueryable<VisitDetail> GetVisits([Service] IVisitRepository repo)
     {
         var visits = repo.GetAll();
         var details = visits.ProjectTo<VisitDetail>(VisitMap.GetConfiguration());
         return details;
     }
-
-
-    /*public async Task<IEnumerable<VisitDetail>> VisitsAsync([Service] IVisitRepository repo, [Service] IMapper mapper, VisitFilter filter, CancellationToken cancellationToken)
-    {
-        var visits = await repo.GetVisits(filter, cancellationToken);
-        return mapper.Map<ICollection<VisitDetail>>(visits);
-    }
-
-    public async Task<VisitDetail> VisitAsync([Service] IVisitRepository repo, [Service] IMapper mapper, string email, CancellationToken cancellationToken)
-    {
-        var visit = await repo.GetVisitByVisitorEmail(email, cancellationToken);
-        return mapper.Map<VisitDetail>(visit);
-    }*/
 }
